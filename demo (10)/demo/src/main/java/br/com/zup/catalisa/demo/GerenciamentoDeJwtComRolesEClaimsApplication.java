068625443e4c1a81e99581784d16ca2aec6ff841
Fabricio John Sullivan <fabricio.sullivan@zup.com.br>
2025-02-23 11:20:01 -0300

    Inserindo o "corpo" do projeto

---
+++ demo (10)/demo/src/main/java/br/com/zup/catalisa/demo/GerenciamentoDeJwtComRolesEClaimsApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.catalisa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeJwtComRolesEClaimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeJwtComRolesEClaimsApplication.class, args);
+	}
+
+}
---
+++ demo (10)/demo/src/test/java/br/com/zup/catalisa/demo/GerenciamentoDeJwtComRolesEClaimsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.catalisa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeJwtComRolesEClaimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
